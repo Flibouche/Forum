1c9a1e780652e53f05e65a3e131bb3fb452b9a89
Flibouche <kevin.pfiffer2@gmail.com>
2024-05-19 23:54:34 +0200

    Try to make a topic.

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -2,9 +2,45 @@
 $categories = $result["data"]['categories'];
 ?>
 
-<h1>Liste des catégories</h1>
+<section>
 
-<?php
-foreach ($categories as $category) { ?>
-    <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getName() ?></a></p>
-<?php }
+    <div>
+
+
+        <h1>Liste des catégories</h1>
+
+        <?php
+        foreach ($categories as $category) { ?>
+            <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getName() ?></a></p>
+        <?php } ?>
+
+        <button id="btn-show" onclick="showTopic()">Create a topic</button>
+
+        <form action="" method="POST" id="create-topic">
+            <div class="form__group">
+                <label for="title">Enter a title for the topic</label>
+                <input type="title" name="title" id="title" aria-label="Topic's Title">
+            </div>
+
+            <div class="form__group">
+                <textarea class="post">Contenu du post à éditer</textarea>
+            </div>
+
+            <button id="btn-topic">Create the topic</button>
+        </form>
+
+    </div>
+
+</section>
+
+<script>
+    let topic = document.getElementById("create-topic");
+    topic.style.display = "none";
+    // let btn = document.getElementById("btn-show");
+
+    showTopic() {
+        topic.style.display = "true";
+    }
+
+
+</script>
